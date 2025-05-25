b88e1a3f8729d62ef5b1897de45a4360d219fe1b
John Lloyd Lawas <jlawas06@gmail.com>
2025-05-26 00:23:31 +0800

    fix: datetime filter issue

--- src/routes/api/get-current-week-schedule/+server.ts
+++ src/routes/api/get-current-week-schedule/+server.ts
@@ -4,7 +4,12 @@ import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async () => {
     const currentDate = new Date();
-    const currentDateFormatted = currentDate.toISOString().split('T')[0];
+    const currentDateFormatted = currentDate.getFullYear() + '-' + 
+        String(currentDate.getMonth() + 1).padStart(2, '0') + '-' + 
+        String(currentDate.getDate()).padStart(2, '0') + ' ' +
+        String(currentDate.getHours()).padStart(2, '0') + ':' +
+        String(currentDate.getMinutes()).padStart(2, '0') + ':' +
+        String(currentDate.getSeconds()).padStart(2, '0');
 	const pocketbaseAdminClient = await getPocketbaseAdminClient();
 	const currentWeekSchedule = await pocketbaseAdminClient
 		.collection('weekly_schedules')
